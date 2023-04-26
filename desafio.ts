49a9f571c95f0a6168922afead3ed6d35ad99985
João Arthur Silva <126902720+JoaoArthur25@users.noreply.github.com>
2023-04-26 19:44:57 -0300

    Update desafio.ts

--- desafio.ts
+++ desafio.ts
@@ -1,79 +1,85 @@
 class ContaBancaria {
 
-    public numeroConta: number;
-    public titular: string;
-    public saldo: number;
-  
-    constructor(numeroConta: number, saldo: number, titular: string) {
+  public numeroConta: number;
+  public titular: string;
+  public saldo: number;
 
-      this.numeroConta = numeroConta;
-      this.saldo = saldo;
-      this.titular = titular;
-    }
-  
-    depositar(valor: number){
+  constructor(numeroConta: number, saldo: number, titular: string) {
 
-        if(valor > 0){
-
-            this.saldo += valor;
-    }
-        else{
-
-            console.log("Valor inválido");
-        }
-    }
-
-    sacar(valor: number){
-
-        if(valor > this.saldo){
-            console.log("Saldo insuficiente");
-        }
-        else if(valor <= 0){
-            console.log("Valor inválido");
-        }
-        else{
-            this.saldo -= valor;
-        }
-    }
-
-    consultarSaldo(){
-
-        return console.log('O saldo atual é: R$' + this.saldo);
-    }
-
-    static validarNumero(numero: string): boolean {
-
-        if (numero.length !== 6) {
-          return false;
-        }
-    
-        for (let i = 0; i < numero.length; i++) {
-          if (isNaN(parseInt(numero[i], 10))) {
-            return false;
-          }
-        }
-
-        return true;
-      }
+    this.numeroConta = numeroConta;
+    this.saldo = saldo;
+    this.titular = titular;
   }
-  
-    const conta1 = new ContaBancaria(123456, 1000, "João Arthur");
-    const conta2 = new ContaBancaria(654321, 2500, "Rian Ernesto");
 
-    console.log(conta1);
-    console.log(conta2);
+  depositar(valor: number): string {
 
-    console.log(ContaBancaria.validarNumero('123456'));
-    console.log(ContaBancaria.validarNumero('123ab6'));
+    if (valor > 0) {
 
-    conta1.depositar(500);
-    conta1.consultarSaldo();
+      return `${this.saldo += valor}`;
+    }
+    else {
 
-    conta1.sacar(200);
-    conta1.consultarSaldo();
+      return "Valor inválido";
+    }
+  }
 
-    conta1.sacar(2000);
-    conta1.consultarSaldo();
-    
-    conta2.depositar(5000);
-    conta2.consultarSaldo();
+  sacar(valor: number): string {
+
+    if (valor <= this.saldo && valor > 0) {
+      this.saldo -= valor;
+      return `Saque de ${valor} realizado com sucesso. Novo saldo: ${this.saldo}`;
+    }
+    else if (valor <= 0) {
+      return "Valor inválido";
+    }
+    else if (valor > this.saldo){
+
+      return "Saldo insuficiente";
+    }
+    else{
+        
+        return "Valor inválido";
+    }
+  }
+
+  consultarSaldo(): string {
+
+    return `O saldo atual é: R$ ${this.saldo}`;
+  }
+
+  static validarNumero(numero: string): boolean {
+
+    if (numero.length !== 6) {
+      return false;
+    }
+
+    for (let i = 0; i < numero.length; i++) {
+      if (isNaN(parseInt(numero[i], 10))) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+}
+
+const conta1 = new ContaBancaria(123456, 1000, "João Arthur");
+const conta2 = new ContaBancaria(654321, 2500, "Rian Ernesto");
+
+console.log(conta1);
+console.log(conta2);
+
+console.log(ContaBancaria.validarNumero('123456'));
+console.log(ContaBancaria.validarNumero('123ab6'));
+
+conta1.depositar(500);
+console.log(conta1.consultarSaldo());
+
+conta1.sacar(200);
+console.log(conta1.consultarSaldo());
+
+console.log(conta1.sacar(2000));
+console.log(conta1.consultarSaldo());
+
+conta2.depositar(5000);
+console.log(conta2.consultarSaldo());
